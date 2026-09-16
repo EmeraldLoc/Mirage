bd1a7c9bdc5f3bec0487db22e65eab61f7772358
xLuigiGamerx <88401287+xLuigiGamerx@users.noreply.github.com>
2026-09-16 17:43:30 +0300

    Color code support for log

--- src/log.hpp
+++ src/log.hpp
@@ -2,20 +2,74 @@
 
 #include <iostream>
 #include <format>
+#include <regex>
 #include <string>
+
+#ifdef _WIN32
+#include <windows.h>
+
+// Enables ANSI/VT escape sequences on Windows consoles
+inline void enableVTMode(void) {
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE) return;
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode)) return;
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+}
+#else
+inline void enableVTMode(void) {}
+#endif
 
 namespace Logging {
     template <typename... Args>
     void log(const std::string &title, std::format_string<Args...> fmt, Args &&...args) {
+        static bool vtInit = (enableVTMode(), true);
+
         std::string str = std::format(fmt, std::forward<Args>(args)...);
 
-        for (size_t pos = 0; (pos = str.find("\\#", pos)) != std::string::npos;) {
-            const auto end = str.find('\\', pos + 2);
-            if (end == std::string::npos) break;
-    
-            str.erase(pos, end - pos + 1);
+        std::regex pattern(R"(\\#(?:([0-9a-fA-F]{6})|([0-9a-fA-F]{3}))\\|(\\#\\))");
+
+        auto it = std::sregex_iterator(str.begin(), str.end(), pattern);
+        auto end = std::sregex_iterator();
+
+        std::string result;
+        size_t lastPos = 0;
+        bool colorApplied = false;
+
+        for (; it != end; it++) {
+            const std::smatch &match = *it;
+            result.append(str, lastPos, match.position() - lastPos);
+
+            uint8_t r = 0, g = 0, b = 0;
+
+            if (match[1].matched) {
+                std::string hex = match[1].str();
+                r = std::stoi(hex.substr(0, 2), nullptr, 16);
+                g = std::stoi(hex.substr(2, 2), nullptr, 16);
+                b = std::stoi(hex.substr(4, 2), nullptr, 16);
+            } else if (match[2].matched) {
+                std::string hex = match[2].str();
+                r = std::stoi(std::string(2, hex[0]), nullptr, 16);
+                g = std::stoi(std::string(2, hex[1]), nullptr, 16);
+                b = std::stoi(std::string(2, hex[2]), nullptr, 16);
+            } else if (match[3].matched) {
+                r = g = b = 0xdc;
+            }
+
+            std::ostringstream ansiSeq;
+            ansiSeq << "\e[38;2;" << static_cast<int>(r) << ";" << static_cast<int>(g) << ";" << static_cast<int>(b) << "m";
+            result.append(ansiSeq.str());
+            colorApplied = true;
+
+            lastPos = match.position() + match.length();
+        }
+        result.append(str, lastPos, std::string::npos);
+
+        if (colorApplied) {
+            result.append("\033[0m");
         }
 
-        std::cout << std::format("[{}] {}\n", title, str);
+        std::cout << std::format("[{}] {}\n", title, result);
     }
 }
